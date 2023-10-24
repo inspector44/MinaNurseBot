1d1682b511328d54a098b37a0b8819c258f000b1
Hzaugiser <hzaugiser@gmail.com>
2023-10-24 10:18:38 +0800

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -4,8 +4,15 @@
 TOKEN="YOUR_TOKEN_HERE"
 CHAT_ID="YOUR_CHAT_ID_HERE"
 
+ip=$(curl http://api.ipify.org)
+
 # Get the status and assign it to the result variable
-result=$(/usr/local/bin/mina client status | grep -E "Block height:|Local uptime:|Peers:|Sync status:|Block producers running:|Next block will be produced in:|External IP:")
+result=$(/usr/local/bin/mina client status | grep -E "Sync status:")
 
 # Send the result to the specified Telegram channel
-/usr/bin/curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$result"
+
+
+if [ "$result" != "Sync status:                                   Synced" ]
+then
+        /usr/bin/curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$(hostname) ${ip} 故障"
+fi
